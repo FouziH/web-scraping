6e9e1bd488b81c2159f540bf195737648172d6b7
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-06 22:41:33 -0600

    scraping the gaurdain

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 const PORT = 8000;
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { html } = require('cheerio/lib/api/manipulation');
 const express = require('express')
 
 const app = express()
 
 
+const getScraping = async() => {
+
+    try {
+        const response = await axios('https://www.theguardian.com/uk')
+        const $ = cheerio.load(response.data)
+        $('.fc-item__title', html).each(() =>{
+            $(this).text()
+        })
+        
+        
+    } catch (error) {
+        console.log(error);
+        
+    } 
+    
+}
+getScraping()
+
+
 app.listen(PORT, () => {
     console.log('Server running on port', PORT)
 })
+
+
+
